dd492aa3d10e5ce4e140ea804996b1a54d6a06c8
Quentin Ikeno <ikenoq@gmail.com>
2022-05-03 19:30:33 -1000

    Add test for makeChains method.

---
+++ makeText.test.js
@@ -0,0 +1,18 @@
+const { MarkovMachine } = require("./markov");
+
+describe("Test MarkovMachine class.", () => {
+	let mm;
+	beforeEach(() => {
+		mm = new MarkovMachine("the cat in the hat");
+	});
+
+	test("makeChains should return an object of word chains.", () => {
+		const chains = {
+			the: ["cat", "hat"],
+			cat: ["in"],
+			in: ["the"],
+			hat: [null],
+		};
+		expect(mm.makeChains()).toEqual(chains);
+	});
+});
