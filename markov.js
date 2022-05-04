3b946d4cd52e9878cf7b4b1f9a6575468f63d2a5
Quentin Ikeno <ikenoq@gmail.com>
2022-05-03 19:26:16 -1000

    Implement makeChains() method.

--- markov.js
+++ markov.js
@@ -1,29 +1,39 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
+	/** build markov machine; read in text.*/
 
-  /** build markov machine; read in text.*/
+	constructor(text) {
+		let words = text.split(/[ \r\n]+/);
+		this.words = words.filter((c) => c !== "");
+		this.makeChains();
+	}
 
-  constructor(text) {
-    let words = text.split(/[ \r\n]+/);
-    this.words = words.filter(c => c !== "");
-    this.makeChains();
-  }
+	/** set markov chains:
+	 *
+	 *  for text of "the cat in the hat", chains will be
+	 *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  /** set markov chains:
-   *
-   *  for text of "the cat in the hat", chains will be
-   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+	makeChains() {
+		// TODO
+		let chains = new Object();
+		this.words.forEach((word, index) => {
+			let nextWord;
+			if (!(word in chains)) {
+				chains[word] = [];
+			}
+			nextWord =
+				index + 1 === this.words.length ? null : this.words[index + 1];
+			chains[word].push(nextWord);
+		});
+		return chains;
+	}
 
-  makeChains() {
-    // TODO
-  }
+	/** return random text from chains */
 
-
-  /** return random text from chains */
-
-  makeText(numWords = 100) {
-    // TODO
-  }
+	makeText(numWords = 100) {
+		// TODO
+	}
 }
+
+module.exports = { MarkovMachine };
